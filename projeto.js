20b7364a984ce845d5357f4212512283071e18b1
Lucas Pessoa <lucas.pessoa@bosal.com>
2024-04-15 16:49:39 -0300

    todos atualizados

--- projeto.js
+++ projeto.js
@@ -1,17 +1,17 @@
 let mensagemGanhou = document.querySelector("[data-winning-massage-text]");
 let celulas = document.querySelectorAll("[data-cell]");
-let celulasBotoes = document.querySelectorAll("[button-cell]");
-let reiniciarJogo = document.querySelector("[data-restart-button]")
+let celulasBotoes = document.querySelectorAll("[data-button-cell]");
+let reiniciarJogo = document.getElementById("button");
 
 let botaoSelecionado = null;
 
 const combinacoesVencer = [
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, 9],
+    [7, 8,  ],
     [1, 4, 7],
     [2, 5, 8],
-    [3, 6, 9],
+    [3, 6,  ],
 ];
 
 const movendoBotoes = () => { 
@@ -22,14 +22,21 @@ const movendoBotoes = () => {
         })
     });
     console.log("clicou");
+
+    recebendoBotoes();
 }
 
 const recebendoBotoes = () => {
 
     celulas.forEach(celula => {
         celula.addEventListener('click', () => {
-           botaoSelecionado = botao.cloneNode(true);
-        })
+            if (botaoSelecionado) {
+                if (!celula.querySelector('button')) {
+                    celula.appendChild(botaoSelecionado);
+                    botaoSelecionado = null;
+                }
+            }
+        });
     });
 }
 
@@ -48,6 +55,8 @@ const checandoSeGanhou = () => {
     });
 };
 
-reiniciarJogo.addEventListener("click", embaralhandoBotoes);
+reiniciarJogo.addEventListener("click", function(){
+    embaralhandoBotoes();
+});
 
-recebendoBotoes();
+movendoBotoes();
--- teste.js
+++ teste.js
@@ -1,8 +1,9 @@
 let mensagemGanhou = document.querySelector("[data-winning-massage-text]");
 let celulas = document.querySelectorAll("[data-cell]");
-let celulasBotoes = document.querySelectorAll("[button-cell]");
+let celulasBotoes = document.querySelectorAll("[data-button-cell]");
 let reiniciarJogo = document.querySelector("[data-restart-button]");
 let botaoSelecionado = null;
+
 const combinacoesVencer = [
    [1, 2, 3],
    [4, 5, 6],
@@ -11,36 +12,50 @@ const combinacoesVencer = [
    [2, 5, 8],
    [3, 6, 9],
 ];
-const movendoBotoes = () => {
-   celulasBotoes.forEach(botao => {
-       botao.addEventListener('click', () => {
+
+const apagandoBotoes = () => {
+    celulasBotoes.forEach(botao => {
+        botao.addEventListener('click', () => {
             botao.classList.add("oculto");
+        })
+    })
+}
+
+const movendoBotoes = () => {
+
+    celulasBotoes.forEach(botao => {
+        botao.addEventListener('click', () => {
+            apagandoBotoes();
             botaoSelecionado = botao.cloneNode(true);
         });
-   });
-   celulas.forEach(celula => {
-       celula.addEventListener('click', () => {
-           if (botaoSelecionado) {
-               if (!celula.querySelector('button')) {
-                   celula.appendChild(botaoSelecionado);
-                   botaoSelecionado = null;
-               }
-           }
-       });
-   });
+    });
+    
+    celulas.forEach(celula => {
+        celula.addEventListener('click', () => {
+            if (botaoSelecionado) {
+                if (!celula.querySelector('[data-button-cell]')) {
+                    celula.appendChild(botaoSelecionado);
+                }
+            }
+        });
+    });
 };
+
 const embaralhandoBotoes = () => {
-   celulasBotoes.forEach(botao => {
-       let posicao = Math.floor(Math.random() * 9);
-       botao.style.order = posicao;
-   });
+    celulasBotoes.forEach(botao => {
+        let posicao = Math.floor(Math.random() * 9);
+        botao.style.order = posicao;
+    });
 };
+
 const checandoSeGanhou = () => {
-   return combinacoesVencer.some((combinacao) => {
-       return combinacao.every((index) => {
-           return celulas[index]
-       });
-   });
+    return combinacoesVencer.some((combinacao) => {
+        return combinacao.every((index) => {
+            return celulas[index]
+        });
+    });
 };
+
 reiniciarJogo.addEventListener("click", embaralhandoBotoes);
+
 movendoBotoes();
